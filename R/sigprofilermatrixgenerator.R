6595d75d29305c9584c23cd41e5d07b34b0edf60
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-05-17 18:23:16 -0400

    Mute SigProfilerMatrixGenerator's output

--- R/sigprofilermatrixgenerator.R
+++ R/sigprofilermatrixgenerator.R
@@ -59,7 +59,12 @@ classify.muts <- function(df, genome.string, spectype='SNV',
     close(f)
     options(scipen=old.opt)
 
-    mat <- SigProfilerMatrixGeneratorR(sample.name, genome.string, spmgd, seqInfo=TRUE, plot=save.plot)
+    # Prevent sigprofilermatrixgenerator's output from being printed
+    # XXX: should probably trycatch() here and print the output if an error
+    # is generated
+    spmg.output <- capture.output(
+        mat <- SigProfilerMatrixGeneratorR(sample.name, genome.string, spmgd, seqInfo=TRUE, plot=save.plot)
+    )
 
     # Read in the types
     annot.files <- paste0(spmgd, '/output/vcf_files/', spectype, '/', c(1:22,'X','Y'), "_seqinfo.txt")
