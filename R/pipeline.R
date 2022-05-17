6c921a84bddf049ba598ac50fc1e8c48ed9a0e44
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-05-17 15:09:13 -0400

    Add a convenience function to run the pipeline end-to-end

---
+++ R/pipeline.R
@@ -0,0 +1,78 @@
+run.pipeline <- function(
+    sc.sample, bulk.sample,
+    mmq60, mmq1,
+    hsnps,
+    abfits,
+    sccigars, bulkcigars,
+    genome, grs=tileGenome(seqlengths=seqinfo(genome.string.to.bsgenome.object(genome))[1:22], tilewidth=10e6, cut.last.tile.in.chrome=TRUE),
+    verbose=TRUE)
+{
+    if (missing(grs)) {
+        g
+    }
+    printfun <- invisible
+    if (verbose)
+        printfun <- print
+
+    perfcheck <- function(msg, expr) {
+        t <- system.time(eval(expr))
+        g <- gc(reset=TRUE)
+        sprintf('%30s |  %7.1f %10.1f %7.1f %7.1f', msg,
+            sum(g[,which(colnames(g)=='used')+1]),
+            sum(g[,which(colnames(g)=='max used')+1]),
+            # combine user, system, and child cpu time
+            sum(t[names(t) != 'elapsed']),
+            t['elapsed'])
+    }
+
+    cat('Starting chunked SCAN2 pipeline on', length(grs), 'chunks\n')
+    cat('Parallelizing with', future::availableCores(), 'cores\n')
+    cat('Detailed chunk schedule:\n')
+    cat(sprintf('%7s %5s %10s %10s\n', 'Chunk', 'Chr', 'Start', 'End'))
+    for (i in 1:length(grs)) {
+        cat(sprintf('%7d %5s %10d %10d\n', i, seqnames(grs[[i]])[1],
+            start(grs[[i]]), end(grs[[i]])))
+    }
+
+    progressr::with_progress({
+        p <- progressr::progressor(along=1:length(grs))
+        p(amount=0, class='sticky',
+            sprintf('%30s | %9s %11s %9s %9s', 'Step (chunk)', 'Mem Mb', 'Peak mem Mb', 'Time (s)', 'Elapsed'))
+        xs <- future.apply::future_lapply(1:length(grs), function(i) {
+            gr <- grs[[i]]
+            p(class='sticky', amount=0, perfcheck(paste('make.scan',i),
+                gt <- make.scan(single.cell=sc.sample, bulk=bulk.sample, genome=genome, region=gr)))
+            gt <- add.static.filter.params(gt)
+            p(class='sticky', amount=0, perfcheck(paste('read.gatk',i),
+                x1 <- read.gatk(gt, path=mmq60, quiet=!verbose)))
+            p(class='sticky', amount=0, perfcheck(paste('read.gatk.lowmq',i),
+                y1 <- read.gatk.lowmq(x1, path=mmq1, quiet=!verbose)))
+            p(class='sticky', amount=0, perfcheck(paste('add.training.data',i),
+                z1 <- add.training.data(y1, path=hsnps, quiet=!verbose)))
+            p(class='sticky', amount=0, perfcheck(paste('add.ab.fits',i),
+                w1 <- add.ab.fits(z1, path=abfits)))
+            p(class='sticky', amount=0, perfcheck(paste('compute.ab.estimates',i),
+                v1 <- compute.ab.estimates(w1, quiet=!verbose)))
+            p(class='sticky', amount=0, perfcheck(paste('add.cigar.data',i),
+                r1 <- add.cigar.data(v1, sccigars, bulkcigars, quiet=!verbose)))
+            p(class='sticky', amount=0, perfcheck(paste('compute.models',i),
+                s1 <- compute.models(r1)))
+            p()
+            s1
+        })
+    })
+
+    x <- do.call(concat, xs)
+    printfun(system.time(x2 <- resample.training.data(x)))
+    printfun('After resample.training.data:\n') ; printfun(gc(reset=TRUE))
+    printfun(system.time(x4 <- compute.excess.cigar.scores(x2)))
+    printfun('After compute.excess.cigar.scores:\n') ; printfun(gc(reset=TRUE))
+    printfun(system.time(x5 <- compute.static.filters(x4)))
+    printfun('After compute.static.filters:\n') ; printfun(gc(reset=TRUE))
+    printfun(system.time(x6 <- compute.fdr.priors(x5)))
+    printfun('After compute.fdr.priors:\n') ; printfun(gc(reset=TRUE))
+    printfun(system.time(x7 <- compute.fdr(x6)))
+    printfun('After compute.fdr:\n') ; printfun(gc(reset=TRUE))
+
+    list(xs, x7)
+}
