26b0cd965f6695b599d58e674ea1c74467293364
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-05-19 18:58:56 -0400

    Break out perfcheck() into a globally-available function

--- R/pipeline.R
+++ R/pipeline.R
@@ -1,3 +1,22 @@
+# Run `expr` and then print a few statistics about memory and runtime
+# print.header - if TRUE, msg and expr are ignored.  It does NOT mean
+#     print msg/expr and ADD a header!
+perfcheck <- function(msg, expr, print.header=FALSE) {
+    if (print.header) {
+        return(sprintf('%30s | %9s %11s %9s %9s',
+            'Step (chunk)', 'Mem Mb', 'Peak mem Mb', 'Time (s)', 'Elapsed'))
+    }
+    t <- system.time(eval(expr))
+    g <- gc(reset=TRUE)
+    sprintf('%30s |  %7.1f %10.1f %7.1f %7.1f', msg,
+        sum(g[,which(colnames(g)=='used')+1]),
+        sum(g[,which(colnames(g)=='max used')+1]),
+        # combine user, system, and child cpu time
+        sum(t[names(t) != 'elapsed']),
+        t['elapsed'])
+}
+
+
 run.pipeline <- function(
     sc.sample, bulk.sample,
     mmq60, mmq1,
@@ -11,22 +30,6 @@ run.pipeline <- function(
     cat('Setting OpenBLAS corecount to 1. This prevents multithreaded matrix multiplication in chunks where it is undesired.\n')
     RhpcBLASctl::blas_set_num_threads(1)
     cat('Parallelizing with', future::availableCores(), 'cores\n')
-
-    printfun <- invisible
-    if (verbose)
-        printfun <- print
-
-    perfcheck <- function(msg, expr) {
-        t <- system.time(eval(expr))
-        g <- gc(reset=TRUE)
-        sprintf('%30s |  %7.1f %10.1f %7.1f %7.1f', msg,
-            sum(g[,which(colnames(g)=='used')+1]),
-            sum(g[,which(colnames(g)=='max used')+1]),
-            # combine user, system, and child cpu time
-            sum(t[names(t) != 'elapsed']),
-            t['elapsed'])
-    }
-
     cat('Detailed chunk schedule:\n')
     cat(sprintf('%7s %5s %10s %10s\n', 'Chunk', 'Chr', 'Start', 'End'))
     for (i in 1:length(grs)) {
@@ -36,10 +39,11 @@ run.pipeline <- function(
 
     progressr::with_progress({
         p <- progressr::progressor(along=1:length(grs))
-        p(amount=0, class='sticky',
-            sprintf('%30s | %9s %11s %9s %9s', 'Step (chunk)', 'Mem Mb', 'Peak mem Mb', 'Time (s)', 'Elapsed'))
+        p(amount=0, class='sticky', perfcheck(print.header=TRUE))
         xs <- future.apply::future_lapply(1:length(grs), function(i) {
             gr <- grs[i,]
+            # Don't put the perfcheck() calls in p(), because progressr
+            # doesn't evaluate those arguments if progress bars re turned off.
             pc <- perfcheck(paste('make.scan',i),
                 gt <- make.scan(single.cell=sc.sample, bulk=bulk.sample, genome=genome, region=gr))
             p(class='sticky', amount=0, pc)
@@ -81,6 +85,9 @@ run.pipeline <- function(
     cat("Chunked pipeline complete.\n")
 
     x <- do.call(concat, xs)
+    cat("Merged SCAN2 object after chunked pipeline:\n")
+    print(x)
+
     cat(perfcheck('resample.training.data', x2 <- resample.training.data(x)), '\n')
     cat(perfcheck('compute.excess.cigar.scores', x4 <- compute.excess.cigar.scores(x2)), '\n')
     cat(perfcheck('compute.static.filters', x5 <- compute.static.filters(x4)), '\n')
