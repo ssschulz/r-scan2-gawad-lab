c8660b563500f85ffae9ca23ebb6d3053a266c64
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-05-28 19:07:36 -0400

    read.tabix.data: support the case where the specified region requests chromosomes not in the tabix file. Tabix throws an error, probably because this usually reflects improper chrom naming (e.g., chr1 vs 1), but in our case we want to allow genomic intervals used for parallelization to cover the entire genome even if the input tables do not.

--- R/tabix.R
+++ R/tabix.R
@@ -20,8 +20,26 @@ read.tabix.data <- function(path, tf, region=NULL,
 
     if (is.null(region))
         data <- Rsamtools::scanTabix(tf)[[1]]
-    else
-        data <- Rsamtools::scanTabix(tf, param=region)[[1]]
+    else {
+        # Important: if a chromosome is requested that isn't in the Tabix file,
+        # then instead of returning empty data it throws an error. The behavior
+        # we'd prefer is to return a 0-row table with the same format that would
+        # otherwise be returned.
+        chrs.in.file <- Rsamtools::seqnamesTabix(path)
+        chrs.in.region <- unique(seqnames(region))
+        if (!all(chrs.in.region %in% chrs.in.file)) {
+            # XXX: This (and many other things that interact with region) only
+            # works for a single genomic interval.
+            if (length(chrs.in.region) > 1)
+                stop('read.tabix.data: multi-interval regions are not robustly supported. Some chromosomes in the requested region do not exist in the tabix file')
+            else {
+                data <- c()
+            }
+        } else {
+            # otherwise, all regions were in the tabix file. go ahead with reading
+            data <- Rsamtools::scanTabix(tf, param=region)[[1]]
+        }
+    }
 
     # If there are no lines in the tabix file corresponding to region (or jsut
     # no lines at all), then scanTabix returns character(0). fread() doesn't
