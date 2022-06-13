5321d18d2650e90cf4e577a2ad1163c00fa6f6b3
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-06-13 16:21:51 -0400

    Add some safeguards to handle cases with very low somatic mutation calls and/or germline reference sites

--- R/mutburden.R
+++ R/mutburden.R
@@ -27,40 +27,52 @@ setMethod("compute.mutburden", "SCAN2", function(object, gbp.per.genome=get.gbp.
     
         # germline sites
         g <- object@gatk[resampled.training.site == TRUE & muttype == mt]
-        # somatic sites
-        s <- object@gatk[pass == TRUE & muttype == mt]
+        # these computations rely on there being a reasonably large number
+        # of germline sites tested. even 100 is very few; we expect more like
+        # 100,000.
+        if (nrow(g) < 100) {
+            warning(paste('only', nrow(g), 'resampled germline', mt, 'sites were detected; aborting genome-wide extrapolation. Typical whole-genome experiments include ~10-100,000 germline sites'))
+            ret <- data.frame(
+                ncalls=NA,
+                callable.sens=NA,
+                callable.bp=sapply(split(dptab[,-(1:sfp$min.bulk.dp)], rowqs), sum)
+            )
+        } else {
+            # somatic sites
+            s <- object@gatk[pass == TRUE & muttype == mt]
 
-        # Break data into 4 quantiles based on depth, use the middle 2 (i.e.,
-        # middle 50%) to reduce noise caused by very low and very high depth.
-        q=4
-        qbreaks <- quantile(g$dp, prob=0:q/q)
+            # Break data into 4 quantiles based on depth, use the middle 2 (i.e.,
+            # middle 50%) to reduce noise caused by very low and very high depth.
+            q=4
+            qbreaks <- quantile(g$dp, prob=0:q/q)
     
-        # s also uses g-based depth quantiles
-        s$dpq <- cut(s$dp, qbreaks, include.lowest=T, labels=F)
-        s$dpq[s$dpq==3] <- 2 # merge 25-75% into a single bin
-        g$dpq <- cut(g$dp, qbreaks, include.lowest=T, labels=F)
-        g$dpq[g$dpq==3] <- 2
+            # s also uses g-based depth quantiles
+            s$dpq <- cut(s$dp, qbreaks, include.lowest=T, labels=F)
+            s$dpq[s$dpq==3] <- 2 # merge 25-75% into a single bin
+            g$dpq <- cut(g$dp, qbreaks, include.lowest=T, labels=F)
+            g$dpq[g$dpq==3] <- 2
 
-        # select the subset of the depth profile passing the bulk depth requirement
-        # cut down dptab to the max value in g$dp (+1 because 1 corresponds to dp=0)
-        dptab <- object@depth.profile$dptab
-        dptab <- dptab[1:min(max(g$dp)+1, nrow(dptab)),]
-        rowqs <- cut(0:(nrow(dptab)-1), qbreaks, include.lowest=T, labels=F)
-        rowqs[rowqs==3] <- 2
-
-        qstouse <- c(1,2,4)
-        s <- s[dpq %in% qstouse]
-        g <- g[dpq %in% qstouse]
-
-        # this data.frame has 1 row for each quantile. the second row (=middle 50%)
-        # is ultimately what we're interested in, but having the other calculations
-        # around can also be interesting.
-        ret <- data.frame(
-            ncalls=s[, sum(pass, na.rm=TRUE), by=dpq],
-            callable.sens=g[bulk.dp >= sfp$min.bulk.dp,
-                mean(resampled.training.pass, na.rm=TRUE), by=dpq],
-            callable.bp=sapply(split(dptab[,-(1:sfp$min.bulk.dp)], rowqs), sum)
-        )
+            # select the subset of the depth profile passing the bulk depth requirement
+            # cut down dptab to the max value in g$dp (+1 because 1 corresponds to dp=0)
+            dptab <- object@depth.profile$dptab
+            dptab <- dptab[1:min(max(g$dp)+1, nrow(dptab)),]
+            rowqs <- cut(0:(nrow(dptab)-1), qbreaks, include.lowest=T, labels=F)
+            rowqs[rowqs==3] <- 2
+    
+            qstouse <- c(1,2,4)
+            s <- s[dpq %in% qstouse]
+            g <- g[dpq %in% qstouse]
+    
+            # this data.frame has 1 row for each quantile. the second row (=middle 50%)
+            # is ultimately what we're interested in, but having the other calculations
+            # around can also be interesting.
+            ret <- data.frame(
+                ncalls=sapply(qstouse, function(q) sum(s[dpq == q]$pass, na.rm=TRUE)),
+                callable.sens=g[bulk.dp >= sfp$min.bulk.dp,
+                    mean(resampled.training.pass, na.rm=TRUE), by=dpq],
+                callable.bp=sapply(split(dptab[,-(1:sfp$min.bulk.dp)], rowqs), sum)
+            )
+        }
 
         ret$callable.burden <- ret$ncalls / ret$callable.gsens
         # dividing by 2 makes it haploid gb
