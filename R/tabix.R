01ec29bee3623a81c5d7638d213d62f1717866aa
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-06-15 11:44:37 -0400

    Support region=NULL in tabix reader

--- R/tabix.R
+++ R/tabix.R
@@ -18,27 +18,36 @@ tabix.read.only.cols <- function(path, header, colClasses, region=NULL) {
     if (!is.null(region) & length(region) > 1)
         stop('region must contain only a single interval')
 
-    command <- paste('tabix', path)
-    if (!is.null(region))
-        command <- paste(command,
-            sprintf('%s:%d-%d', seqnames(region)[1], start(region)[1], end(region)[1]))
+    # As far as I know, there is no way or the tabix command to return
+    # the entire file. So when region is null, we can't use the same
+    # tabix system call method. This is unfortunate since it'd be nice to
+    # not have special behavior when region=NULL, but I suppose it can't
+    # be avoided.
+    if (is.null(region)) {
+        command <- paste('gunzip -c', path)
+    } else {
+        command <- paste('tabix', path)
+        if (!is.null(region))
+            command <- paste(command,
+                sprintf('%s:%d-%d', seqnames(region)[1], start(region)[1], end(region)[1]))
 
-    # handle two types of colClasses:
-    #  1. character vector of types, 1 per column. If any column is not specified
-    #     then it is not read.
-    #  2. list of column types with names in the form of, e.g., character=c('column 1', 'column 7', ...)
-    #     IMPORTANTLY: it doesn't matter what is specified in the list form; we DO NOT ALLOW
-    #     NULL SKIPPING in the list form. So all columns will be read.
-    if (!is.null(colClasses) & !is.list(colClasses)) {
-        if (is.character(colClasses)) {
-            new.col.classes <- rep('NULL', length(header))
-            new.col.classes[1:length(colClasses)] <- colClasses
-        } else {
-            stop('colClasses must either be a character vector or a list')
-        }
-        cut.command <- paste0('cut -f', paste(which(new.col.classes != 'NULL'), collapse=','))
-        command <- paste(command, "|", cut.command)
-    } 
+        # handle two types of colClasses:
+        #  1. character vector of types, 1 per column. If any column is not specified
+        #     then it is not read.
+        #  2. list of column types with names in the form of, e.g., character=c('column 1', 'column 7', ...)
+        #     IMPORTANTLY: it doesn't matter what is specified in the list form; we DO NOT ALLOW
+        #     NULL SKIPPING in the list form. So all columns will be read.
+        if (!is.null(colClasses) & !is.list(colClasses)) {
+            if (is.character(colClasses)) {
+                new.col.classes <- rep('NULL', length(header))
+                new.col.classes[1:length(colClasses)] <- colClasses
+            } else {
+                stop('colClasses must either be a character vector or a list')
+            }
+            cut.command <- paste0('cut -f', paste(which(new.col.classes != 'NULL'), collapse=','))
+            command <- paste(command, "|", cut.command)
+        } 
+    }
 
     system(command, intern=TRUE)
 }
