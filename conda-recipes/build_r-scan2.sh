baf0b59f57f1790b04fe9f3e1204ca7ca0457d2f
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-08-05 14:11:39 -0400

    Bump build

--- conda-recipes/build_r-scan2.sh
+++ conda-recipes/build_r-scan2.sh
@@ -2,4 +2,4 @@
 
 conda-build -c conda-forge -c bioconda -c jluquette r-scan2
 
-anaconda upload  /home/ljl11/miniconda3_for_building/conda-bld/linux-64/r-scan2-1.0-py38r41h3fd9d12_4.tar.bz2
+anaconda upload  /home/ljl11/miniconda3_for_building/conda-bld/linux-64/r-scan2-1.0-py38r41h3fd9d12_5.tar.bz2
