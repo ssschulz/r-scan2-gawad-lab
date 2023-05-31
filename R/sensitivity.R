58fe957437916b7b073bdcdbb78b335f016c7d0f
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2023-05-31 04:04:26 -0400

    Finalize spatial sensitivity model and use it to provide an alternative total mutation burden estimate (EXPERIMENTAL)!  The model seems reasonably accurate.

--- R/sensitivity.R
+++ R/sensitivity.R
@@ -1,45 +1,11 @@
-# tilewidth purposefully not exposed here
-compute.hsnp.spatial.sens <- function(object, muttype=c('snv', 'indel'), quiet=TRUE) {
-    muttype <- match.arg(muttype)
-
-    if (!quiet) cat("Tiling genome with 10 kb tiles\n")
-    tiles <- tileGenome(seqlengths=seqlengths(results@region), tilewidth=1e3, cut.last.tile.in.chrom=T)
-
-    # For hSNP sensitivity with 10kb windows, acf() suggests useful info only
-    # exists for the nearest tile up- or downstream. Even so, the correlation
-    # is only 0.1.
-    compute.hsnp.sens.for.tiles(object, tiles, smooth.tiles=1, muttype=muttype, quiet=quiet)
-}
-
-
-compute.hsnp.sens.for.tiles <- function(object, tiles, smooth.tiles=0, muttype=c('snv', 'indel'), quiet=TRUE) {
-    muttype <- match.arg(muttype)
-
-    # neither muttype == muttype nor muttype == ..muttype work. whatever.
-    different.variable.name <- muttype
-    hsnps <- object@gatk[training.site == TRUE & muttype == different.variable.name]
-    gr <- GRanges(seqnames=hsnps$chr, ranges=IRanges(start=hsnps$pos, width=1),
-        seqinfo=object@genome.seqinfo)
-    gr$pass <- hsnps$training.pass
-
-    if (!quiet) cat(paste0("Mapping ", nrow(hsnps), " germline ", muttype, "s to ", length(tiles), " non-overlapping tiles\n"))
-    tiles$n.called <- countOverlaps(tiles, gr[gr$pass == TRUE])
-    tiles$n.tested <- countOverlaps(tiles, gr)
-    # this will be 0, just to allow for smoothing, then set to NA
-    tiles$sens <- ifelse(tiles$n.tested > 0, tiles$n.called/tiles$n.tested, 0)
-    tiles$sens.smoothed <- as.numeric(stats::filter(tiles$sens,
-        filter=c(1, rep(1, 2*smooth.tiles))/(1+2*smooth.tiles), sides=2))
-    tiles$sens <- ifelse(tiles$n.tested > 0, tiles$sens, NA)
-    tiles
-}
 
 
 
 # Spatial sensitivity applies only to VAF-based calling! Not to mutation signature-based rescue!
 # Use small tiles for parallelization (~1MB) because of basepair resolution.
 compute.spatial.sensitivity.depth <- function(single.cell.id, bulk.id,
-    static.filter.params, joint.dptab.path, genome.string,
-    grs.for.sens=genome.string.to.tiling(genome.string, tilewidth=1e3, group='auto'),
+    static.filter.params, joint.dptab.path, genome.string, sens.tilewidth,
+    grs.for.sens=genome.string.to.tiling(genome.string, tilewidth=sens.tilewidth, group='auto'),
     grs.for.parallelization=genome.string.to.tiling(genome.string, tilewidth=1e6, group='auto'),
     quiet=TRUE, report.mem=TRUE)
 {
@@ -117,8 +83,8 @@ compute.spatial.sensitivity.depth <- function(single.cell.id, bulk.id,
 # core implementation copies the entire parent memory space to each child thread no matter how I
 # try to prevent it. For human genomes, this means a waste of ~2-2.5G of RAM per thread.
 compute.spatial.sensitivity.abmodel <- function(
-    single.cell.id, ab.fits, integrated.table.path, genome.string,
-    grs.for.sens=genome.string.to.tiling(genome.string, tilewidth=1e3, group='auto'),
+    single.cell.id, ab.fits, integrated.table.path, genome.string, sens.tilewidth=1e3,
+    grs.for.sens=genome.string.to.tiling(genome.string, tilewidth=sens.tilewidth, group='auto'),
     grs.for.parallelization=genome.string.to.tiling(genome.string, tilewidth=10e6, group='auto'),
     quiet=TRUE, report.mem=TRUE)
 {
@@ -167,16 +133,190 @@ compute.spatial.sensitivity.abmodel <- function(
 }
 
 
+model.somatic.sensitivity <- function(tiles, muttype=c('snv', 'indel'), alleletype=c('maj', 'min'), random.seed=0) {
+    muttype <- match.arg(muttype)
+    alleletype <- match.arg(alleletype)
 
+    # only use the columns corresponding to this muttype/alleletype
+    tstr <- paste0(muttype, '.n.training.', alleletype)
+    pstr <- paste0(muttype, '.n.training.passed.', alleletype)
+    bsc <- paste0('bases.gt.', muttype, '.sc.min.dp')
+    bblk <- paste0('bases.gt.', muttype, '.bulk.min.dp')
+    nbr <- paste0(muttype, '.n.training.neighborhood')
+
+    # Build model
+    set.seed(random.seed)  # I believe glm is not deterministic. Just be safe.
+    form <- paste0('cbind(', pstr, ', ', tstr, ' - ', pstr, ') ~ abs(gp.mu) + ',
+        paste(c(bsc, bblk, nbr, 'gp.sd', 'mean.sc.dp'), collapse=" + "))
+    model <- glm(form, family=binomial, data=tiles[hold.out == FALSE])
+
+    # Predict on the entire data table (INCLUDING training data) and save predictions
+    # to caller's table by reference.
+    # To ASSESS the model, only consider the predictions on the hold.out==TRUE set.
+    predname <- paste0('pred.', muttype, '.', alleletype)
+    # 1/(1+exp(-x)) is inverse logit to transform -inf..+inf values to probabilities in [0,1]
+    tiles[, (predname) := 1/(1+exp(-predict(object=model, newdata=.SD)))]
+
+    model
+}
+
+
+# convenience function to pare down @spatial.sensitivity to columns relevant
+# only to a specific (muttype, alleletype) combo. the returned data.table will
+# have columns named "pred", "npass", "ntotal" regardless of which (muttype,
+# alleletype) pair was passed.
+#
+# alleletype='both' takes the average predicted sensitivity of major and minor
+#   alleles, representing the assumption that somatic mutations are equally
+#   likely to occur on either allele. this assumption is not always a good one:
+#   e.g., balancer chromosomes, perhaps Barr bodies, etc.
+select.muttype.and.alleletype <- function(object, data=object@spatial.sensitivity$data, muttype=c('snv', 'indel'), alleletype=c('both', 'maj', 'min')) {
+    muttype <- match.arg(muttype)
+    alleletype <- match.arg(alleletype)
     
+    if (alleletype == 'both') {
+        pred.maj <- paste0('pred.', muttype, '.maj')
+        pred.min <- paste0('pred.', muttype, '.min')
+        ret <- data[, .(hold.out,
+            pred=(get(pred.maj) + get(pred.min)) / 2,
+            npass=get(paste0(muttype, '.n.training.passed')),
+            ntotal=get(paste0(muttype, '.n.training')),
+            ncalls=get(paste0(muttype, '.n.calls')))]
+    } else {
+        ret <- data[, .(hold.out,
+            pred=get(paste0('pred.', muttype, '.', alleletype)),
+            npass=get(paste0(muttype, '.n.training.passed.', alleletype)),
+            ntotal=get(paste0(muttype, '.n.training.', alleletype)),
+            ncalls=get(paste0(muttype, '.n.calls.', alleletype)))]
+    }
+    ret
+}
+
+
+assess.predicted.somatic.sensitivity <- function(object, muttype=c('snv', 'indel'), alleletype=c('both', 'maj', 'min'))
+{
+    # For assessing the model, need to use only the held-out tiles that were not
+    # used for training.
+    # ntotal > 0 makes the comparison similar to genome-wide mean germline sens.
+    stratify.tiles.by.somatic.sensitivity(
+        data=object@spatial.sensitivity$data[hold.out == TRUE],
+        muttype=muttype, alleletype=alleletype)
+}
+
+
+# predicted sensitivity is rounded to `stratify.sensitivity.digits` decimals. 2 is
+# good in practice. increasing the digits will use a closer approximation of the
+# model's sensitivity, which could increase accuracy in some cases. however, more
+# digits will also divide the genome into more, finer regions, thereby decreasing
+# the number of somatic calls and germline sites in each region. this reduction in
+# sites increases noise in sensitivity estimates, which may outweigh the increase
+# in accuracy gained by using more digits from the predicted sensitivity.
+stratify.tiles.by.somatic.sensitivity <- function(object, data=object@spatial.sensitivity$data,
+    muttype=c('snv', 'indel'), alleletype=c('both', 'maj', 'min'), stratify.sensitivity.digits=2)
+{
+    ret <- select.muttype.and.alleletype(data=data, muttype=muttype, alleletype=alleletype)
+
+    ret[, .(npass=sum(npass), ntotal=sum(ntotal),
+            sens=sum(npass)/sum(ntotal),
+            ncalls=sum(ncalls),
+            n=nrow(.SD)), by=.(pred=round(pred, stratify.sensitivity.digits))][order(pred)]
+}
+
+
+# as above, when comparing spatial and germline L-O-O estimates of sensitivity,
+# windows with 0 germline sites must be excluded. However, do not need to
+# consider hold.out vs. training.
+# keep.germline.zeros=TRUE may provide a better genome-wide extrapolation of
+# sensitivity because germline L-O-O estimates cannot, by definition, provide
+# information about regions of the genome where no germline variants are
+# present.
+somatic.sensitivity <- function(object, data=object@spatial.sensitivity$data,
+    muttype=c('snv', 'indel'), alleletype=c('both', 'maj', 'min'), keep.germline.zeros=FALSE)
+{
+    if (!missing(object) & !missing(data))
+        stop('exactly one of `object` or `data` may be specified')
+
+    muttype <- match.arg(muttype, several.ok=TRUE)
+    alleletype <- match.arg(alleletype, several.ok=TRUE)
+
+    setNames(lapply(muttype, function(mt) {
+        ret <- do.call(cbind, lapply(alleletype, function(at) {
+            this.ret <- select.muttype.and.alleletype(data=data, muttype=mt, alleletype=at)
+            unlist(this.ret[!is.na(pred) & (keep.germline.zeros | ntotal > 0),
+                .(germline.training=sum(npass)/sum(ntotal),
+                  predicted.somatic.mean=mean(pred, na.rm=TRUE),
+                  predicted.somatic.stdev=sd(pred, na.rm=TRUE),
+                  predicted.somatic.q25=quantile(pred, prob=0.25, na.rm=TRUE),
+                  predicted.somatic.q75=quantile(pred, prob=0.75, na.rm=TRUE))
+            ])
+        }))
+        colnames(ret) <- alleletype
+        ret
+    }), muttype)
+}
+
+
+# Use an EQUAL-BURDEN ASSUMPTION across major and minor alleles to estimate
+# total somatic burden. That is, assume that the same number of mutations
+# occur on the major allele (VAF >= 50%) as the minor allele. This assumption
+# is helpful because sensitivity on the minor allele is much lower than the
+# major allele and leads to noisier estimates. The allele that is major or
+# minor changes with genome location; it does not refer to either the maternal
+# or paternal allele (in humans).
+#
+# N.B. in this context 'major' and 'minor' alleles refer amplification bias:
+# the allele that was more amplified as "major" and less amplified is "minor".
+# there is no differentiation between VAF>=50% due to sampling noise and
+# actual differences in amplification level.
+estimate.burden.by.spatial.sensitivity <- function(object, data=object@spatial.sensitivity$data,
+     muttype=c('snv', 'indel'), n.sims=1e4, stratify.sensitivity.digits=2, random.seed=0)
+{
+    set.seed(random.seed)
+    muttype <- match.arg(muttype)
+
+    # use the model-assigned sensitivity to every tile in the genome to roughly
+    # group the genome by somatic calling sensitivity. once the model's predicted
+    # sensitivity is used to create an aggregated region for each sensitivity value,
+    # the GERMLINE sites in each region are used to calculate sensitivity.
+    #
+    # only the major allele is queried
+    strat.tab <- stratify.tiles.by.somatic.sensitivity(
+        data=data, muttype=muttype, alleletype='maj',
+        stratify.sensitivity.digits=stratify.sensitivity.digits)
+
+    # use a negative binomial process to estimate total number of mutations in
+    # each sensitivity bin (i.e., number called and number missed(=negbin value))
+    sims <- apply(strat.tab[ncalls > 0,.(ncalls, sens)], 1, function(row)
+        row[1] + rnbinom(n=n.sims, size=row[1], prob=row[2]))
+
+    # EQUAL-BURDEN ASSUMPTION: with a decent estimate of the number of mutations
+    # on the VAF>=50% allele, assume the other allele (diploid assumption, obviously)
+    # has the same number.
+    #
+    # each row is one of `n.sims` simulations, each element in the row is the total
+    # number of mutations (called + estimated uncalled(=neg binom)). the sum is the
+    # genomewide burden.
+    sims <- 2*rowSums(sims)
+
+    # return the simulations, metadata and some useful precomputed summaries
+    list(muttype=muttype, n.sims=n.sims, random.seed=random.seed,
+        mean=mean(sims), median=median(sims),
+        ci95=quantile(sims, probs=c(0.025, 0.975)),
+        ci.99=quantile(sims, probs=c(0.005, 0.995)),
+        sims=sims)
+}
+
+
 # sites should be filtered for training.site==TRUE and one muttype
-count.germline.sites.for.sens <- function(grs, sites, seqinfo) {
+# N.B. allele balance is probably a better indicator of underlying
+# amplification imbalance than VAF.
+count.germline.sites.for.sens <- function(grs, sites, seqinfo, neighborhood.tiles) {
     sites.to.gr <- function(sites) {
         GenomicRanges::GRanges(seqnames=sites$chr,
                 ranges=IRanges::IRanges(start=sites$pos, width=1),
                 seqinfo=seqinfo)
     }
-    data.table::data.table(
+    ret <- data.table::data.table(
         n.training=IRanges::countOverlaps(grs,
             sites.to.gr(sites)),
         n.training.maj=IRanges::countOverlaps(grs,
@@ -189,6 +329,26 @@ count.germline.sites.for.sens <- function(grs, sites, seqinfo) {
             sites.to.gr(sites[training.pass == TRUE & af >= 0.5])),
         n.training.passed.min=IRanges::countOverlaps(grs,
             sites.to.gr(sites[training.pass == TRUE & af < 0.5])))
+    ret[, n.training.neighborhood :=
+        as.integer(stats::filter(n.training, filter=rep(1, 2*neighborhood.tiles), sides=2))]
+    ret
+}
+
+
+# should really reuse above code. oh well.
+count.somatic.sites.for.sens <- function(grs, sites, seqinfo, neighborhood.tiles) {
+    sites.to.gr <- function(sites) {
+        GenomicRanges::GRanges(seqnames=sites$chr,
+                ranges=IRanges::IRanges(start=sites$pos, width=1),
+                seqinfo=seqinfo)
+    }
+    ret <- data.table::data.table(
+        n.calls=IRanges::countOverlaps(grs, sites.to.gr(sites)),
+        n.calls.maj=IRanges::countOverlaps(grs, sites.to.gr(sites[af >= 0.5])),
+        n.calls.min=IRanges::countOverlaps(grs, sites.to.gr(sites[af < 0.5])))
+    ret[, n.neighborhood :=
+        as.integer(stats::filter(n.calls, filter=rep(1, 2*neighborhood.tiles), sides=2))]
+    ret
 }
 
 
@@ -196,8 +356,20 @@ count.germline.sites.for.sens <- function(grs, sites, seqinfo) {
 # depth.covs - compute.spatial.sensitivity.depth() output
 # This function doesn't use future() for multicore support, so passing the large
 # SCAN2 `object` is feasible.
+# 
+# WARNING! sens.tilewidth MUST match what was used in depth/abmodel covariate gathering
+#
+# neighborhood.tiles - calculate a rolling sum of the number of training sites using
+#   a window extending `neighborhood.tiles` upstream and downstream of a given tile.
+#   The window will be of size 1 (the center window) + 2*neighborhood.tiles.
+#   This quantifies how much data is available to the AB model in addition to the
+#   standard deviation of the Gaussian process model (gp.sd).
+#   For PTA data, covariance analysis suggests most local AB information is within
+#   10 kB. Thus, with the default sens.tilewidth=1kb, neighborhood.tiles=10 should
+#   be a reasonable default.
 integrate.spatial.sensitivity.covariates <- function(object, abmodel.covs, depth.covs,
-    grs.for.sens=genome.string.to.tiling(object@genome.string, tilewidth=1e3, group='auto'))
+    sens.tilewidth=1e3, neighborhood.tiles=10,
+    grs.for.sens=genome.string.to.tiling(object@genome.string, tilewidth=sens.tilewidth, group='auto'))
 {
     ab.data <- data.table::data.table(chr=as.character(seqnames(grs.for.sens)),
         start=start(grs.for.sens), end=end(grs.for.sens))
@@ -205,20 +377,63 @@ integrate.spatial.sensitivity.covariates <- function(object, abmodel.covs, depth
     cat('Counting germline training hSNP sites..\n')
     hsnps <- count.germline.sites.for.sens(grs=grs.for.sens,
         sites=object@gatk[training.site == TRUE & muttype == 'snv'],
-        seqinfo=genome.string.to.seqinfo.object(object@genome.string))
+        seqinfo=genome.string.to.seqinfo.object(object@genome.string),
+        neighborhood.tiles=neighborhood.tiles)
     colnames(hsnps) <- paste0('snv.', colnames(hsnps))
     ab.data <- cbind(ab.data, hsnps)
+    cat('Counting somatic SNV calls..\n')
+    calls <- count.somatic.sites.for.sens(gr=grs.for.sens,
+        sites=object@gatk[pass == TRUE & muttype == 'snv'],
+        seqinfo=genome.string.to.seqinfo.object(object@genome.string),
+        neighborhood.tiles=neighborhood.tiles)
+    colnames(calls) <- paste0('snv.', colnames(calls))
+    ab.data <- cbind(ab.data, calls)
+
 
     cat('Counting germline training het indel sites..\n')
     hindels <- count.germline.sites.for.sens(grs=grs.for.sens,
         sites=object@gatk[training.site == TRUE & muttype == 'indel'],
-        seqinfo=genome.string.to.seqinfo.object(object@genome.string))
+        seqinfo=genome.string.to.seqinfo.object(object@genome.string),
+        neighborhood.tiles=neighborhood.tiles)
     colnames(hindels) <- paste0('indel.', colnames(hindels))
     ab.data <- cbind(ab.data, hindels)
+    cat('Counting somatic indel calls..\n')
+    calls <- count.somatic.sites.for.sens(gr=grs.for.sens,
+        sites=object@gatk[pass == TRUE & muttype == 'indel'],
+        seqinfo=genome.string.to.seqinfo.object(object@genome.string),
+        neighborhood.tiles=neighborhood.tiles)
+    colnames(calls) <- paste0('indel.', colnames(calls))
+    ab.data <- cbind(ab.data, calls)
 
     cat("Merging with AB model covariates..\n")
     ret <- merge(ab.data, abmodel.covs, by=c('chr', 'start', 'end'))
     cat("Merging with depth covariates..\n")
     ret <- merge(ret, depth.covs, by=c('chr', 'start', 'end'))
-    ret
+
+    cat("Computing sensitivity models\n")
+    # Split data into two halves for later hold-out training
+    ret[, hold.out := rbinom(nrow(.SD), size=1, prob=1/2)]
+    # Each call to model.somatic.sensitivity updates 
+    models <- do.call(c,
+        lapply(c('snv', 'indel'), function(muttype)
+            lapply(c('maj', 'min'), function(alleletype)
+                model.somatic.sensitivity(tiles=ret, muttype=muttype, alleletype=alleletype)
+            )
+        )
+    )
+    names(models) <- c('snv.maj', 'snv.min', 'indel.maj', 'indel.min')
+
+    burdens <- setNames(lapply(c('snv', 'indel'), function(mt)
+        estimate.burden.by.spatial.sensitivity(data=ret, muttype=mt)),
+        c('snv', 'indel'))
+
+    object@spatial.sensitivity <- list(
+        tilewidth=sens.tilewidth,
+        neighborhood.tiles=neighborhood.tiles,
+        models=models,
+        somatic.sensitivity=somatic.sensitivity(data=ret),
+        burden=burdens,
+        data=ret
+    )
+    object
 }
